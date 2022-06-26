a49d9c8ef2e05c94c3da85b9962eb9d4629f4a2c
DESKTOP-UCDIJU3\alina <alinarusanovva@gmail.com>
2022-06-26 11:49:07 +0300

    added class Car

---
+++ LinaTheExplorer/LinaTheExplorer/2. Hi bye device/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hi_bye_device
+{
+    public class Car
+    {
+        public string[] WelcomingMessage { get; init; }
+        public DriversName DriversName { get; init; }
+        public CarModel CarModel { get; init; }
+        public int CarNumber { get; init; }
+        public int DriverWaitingTime { get; init; }
+
+        public Car(string[] welcomingMessage, DriversName driversName, CarModel carModel, int carNumber, int driverWaitingTime)
+        {
+            WelcomingMessage = welcomingMessage;
+            DriversName = driversName;
+            CarModel = carModel;
+            CarNumber = carNumber;
+            DriverWaitingTime = driverWaitingTime;
+
+        }
+
+
+
+
+    }
+}
